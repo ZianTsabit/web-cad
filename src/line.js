a5a331732d1eebab22b30501bc605cae00860be9
ZianTsabit <13520165@std.stei.itb.ac.id>
2023-02-13 03:07:19 +0700

    feat: finalize class for line and state

--- src/line.js
+++ src/line.js
@@ -1,5 +1,28 @@
 /// <reference path="utils.js" />
 
+// function to draw line
+function drawLine(e) {
+    
+    const rect = canvas.getBoundingClientRect();
+    const canvasX = e.clientX - rect.left;
+    const canvasY = e.clientY - rect.top;
+    state.drawAllObjects();
+    if (first) {
+        
+        first = false;
+        p1 = [-1 + 2*canvasX/canvas.width, -1 + 2*(canvas.height-canvasY)/canvas.height];
+
+    } else {
+
+        first = true;
+        p2 = [-1 + 2*canvasX/canvas.width, -1 + 2*(canvas.height-canvasY)/canvas.height];
+
+        state.add(new Line(p1[0], p1[1], p2[0], p2[1])); 
+    }
+    
+    state.drawAllObjects();
+}
+
 class Line {
 
     constructor(x1, y1, x2, y2) {
@@ -10,14 +33,13 @@ class Line {
         this.colorHex = "#000000";
         this.vertices = this.makeVertices();
     }
-
+    
     makeVertices() {
-        
         let vertices = [
-            this.x1, this.y1,
-            this.x2, this.y2
+            [this.x1, this.y1],
+            [this.x2, this.y2]
         ];
-        
+
         return vertices;
     }
 
@@ -25,79 +47,50 @@ class Line {
         this.colorHex = newcolorHex;
     }
 
-
     draw() {
-
-        drawLine(this.vertices, this.colorHex);
-
+        renderLine(this.vertices, this.colorHex);
     }
-
 }
 
-function drawLine(vertices, colorHex) {
-
-    const canvas = document.getElementById('webgl-canvas');
-
-    const gl = canvas.getContext('webgl');
-
-    if (!gl) {
-        alert('WebGL not supported, falling back on experimental-webgl');
-        return;
-    }
-
-    var vbuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    // Create and compile Shader
-
-    var vertCode =
-        'attribute vec2 coordinates;' +
-        'void main(void) {' +
-        ' gl_Position = vec4(coordinates, 0.0, 1.0);' +
-        '}';
-
-    var vertShader = gl.createShader(gl.VERTEX_SHADER);
-
-    gl.shaderSource(vertShader, vertCode);
-
-    gl.compileShader(vertShader);
-
-    var fragCode =
-        'void main(void) {' +
-        ' gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);' +
-        '}';
-
-    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-    gl.shaderSource(fragShader, fragCode);
-    gl.compileShader(fragShader);
-
-    var shaderProgram = gl.createProgram();
-
-    gl.attachShader(shaderProgram, vertShader);
-    gl.attachShader(shaderProgram, fragShader);
-    gl.linkProgram(shaderProgram);
-    gl.useProgram(shaderProgram);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-
-    var coord = gl.getAttribLocation(shaderProgram, "coordinates");
-
-    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
-
-    gl.enableVertexAttribArray(coord);
-
-    gl.clearColor(0.0, 0.0, 0.0, 0.0);
-
-    gl.enable(gl.DEPTH_TEST);
-
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    console.log(canvas.width, canvas.height);
+function renderLine(vertices, colorHex) {
     
-    gl.drawArrays(gl.LINES, 0, vertices.length / 2);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
+    let program = initShader(gl, vertexShaderSrc, fragmentShaderSrc);
+    gl.useProgram(program);
+
+    // Vertex
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    
+    let vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    // Color
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten([toGLSL(colorHex),toGLSL(colorHex)]), gl.STATIC_DRAW);
+
+    let vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.LINES, 0, flatten(vertices).length/2);
+}
+
+function toGLSL(hex){
+    var color;
+    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+        color = hex.substring(1).split('');
+        if(color.length == 3){
+            color = [color[0], color[0], color[1], color[1], color[2], color[2]];
+        }
+        color = '0x' + color.join('');
+        return [((color>>16)&255)/255, ((color>>8)&255)/255, (color&255)/255, 1];
+    }
+    throw new Error('Bad Hex');
 }
