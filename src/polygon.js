092cea478cb91df4853a604f760bbecca9648606
atabiksf <82322722+atabiksf@users.noreply.github.com>
2023-02-13 18:41:57 +0700

    add simple polygon

--- src/polygon.js
+++ src/polygon.js
@@ -1,46 +1,39 @@
 /// <reference path="utils.js" />
 
-let first = true;
-
-let p1, p2;
-
-let vBuffer, cBuffer;
-
-let vertices, colors = [];
+vertices = [];
+colors = [];
 
 /**
-* Menggambar persegi
-* @param {MouseEvent} e 
-*/    
-function drawSquare(e) {
+ * @param {MouseEvent} e 
+ */
+
+var n = 3;
+
+var last_n = 1;
+ 
+function changeN() {
+    n = document.getElementById('n-polygon').value;
+}
+
+function drawPolygon(e) {
     /**@type {HTMLCanvasElement}} */
     const canvas = e.target;
     const rect = canvas.getBoundingClientRect();
     const canvasX = e.clientX - rect.left;
     const canvasY = e.clientY - rect.top;
 
-    if (first) {
-        first = false;
-        
-        p1 = [-1 + 2*canvasX/canvas.width, -1 + 2*(canvas.height-canvasY)/canvas.height];
-    } else {
-        first = true;
-
-        p2 = [-1 + 2*canvasX/canvas.width, -1 + 2*(canvas.height-canvasY)/canvas.height];
-
-        vertices = [
-            p1,
-            [p2[0], p1[1]],
-            [p1[0], p2[1]],
-            p2
-        ];
-
-        colors = [
-            [1, 0, 0, 1],
-            [0, 1, 0, 1],
-            [0, 0, 1, 1],
-            [1, 1, 1, 1]
-        ]
+    if (last_n < n) {
+        console.log(last_n);
+        p = [-1 + 2*canvasX/canvas.width, -1 + 2*(canvas.height-canvasY)/canvas.height];
+        vertices.push(p);
+        colors.push([0,0,0,1]);
+        last_n++;
+    }
+    else {
+        console.log(last_n);
+        p = [-1 + 2*canvasX/canvas.width, -1 + 2*(canvas.height-canvasY)/canvas.height];
+        vertices.push(p);
+        colors.push([0,0,0,1]);
 
         const gl = canvas.getContext("webgl");
 
@@ -63,33 +56,16 @@ function drawSquare(e) {
         cBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-    
+
         var vColor = gl.getAttribLocation(program, "vColor");
         gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vColor);
 
         gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length);
+
+        vertices = [];
+        colors = [];
+        last_n = 1;
     }
 }
-
-const vertexShaderSrc = `precision mediump float;
-
-attribute vec4 vPosition;
-attribute vec4 vColor;
-varying vec4 fColor;
-
-
-void main() {
-    fColor = vColor;
-    gl_Position = vPosition;
-}
-`
-const fragmentShaderSrc = `precision mediump float;
-
-varying vec4 fColor;
-
-void main() {
-    gl_FragColor = fColor;
-}
-`
